8dba5872325bbec7e9990179708fba943b27751d
cSDes1gn <christian911@sympatico.ca>
2021-11-22 17:21:28 -0500

    add asymetric logging for visualizer

--- client/p1/client.py
+++ client/p1/client.py
@@ -1,4 +1,5 @@
 import time
+import random
 import logging
 
 class Client:
@@ -7,9 +8,27 @@ class Client:
 
     def start(self):
         while True:
-            self._logger.debug("debug log")
-            self._logger.info("information log")
-            self._logger.warning("warning log")
-            self._logger.error("error log")
-            self._logger.critical("critical log")
-            time.sleep(5)
+            self.log_debug()
+            self.log_info()
+            self.log_warning()
+            self.log_error()
+
+    def log_info(self):
+        if bool(random.getrandbits(1)):
+            self._logger.info("this is an info log")
+        time.sleep(random.randint(1, 5))
+
+    def log_debug(self):
+        if bool(random.getrandbits(1)):
+            self._logger.debug("this is a debug log")
+        time.sleep(random.randint(1, 5))
+
+    def log_warning(self):
+        if bool(random.getrandbits(1)):
+            self._logger.warning("this is a warning log")
+        time.sleep(random.randint(1, 5))
+
+    def log_error(self):
+        if bool(random.getrandbits(1)):
+            self._logger.error("this is an error log")
+        time.sleep(random.randint(1, 5))
